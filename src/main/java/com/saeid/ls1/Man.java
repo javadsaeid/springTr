7de6992645bff63b2be82ff7b39f01075ef9c23f
javad <javadsaeid8@gmail.com>
2025-03-06 16:05:07 +0330

    clean up

---
+++ src/main/java/com/saeid/ls1/Man.java
@@ -0,0 +1,18 @@
+package com.saeid.ls1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Man {
+    public static void main(String[] args) {
+//        List<String> names = new ArrayList<>();
+
+        List<String> names = new ArrayList<>(2);
+        System.out.println("test");
+//        names.add("John");
+//        names.add("Jane");
+//        names.add("Bob");
+        System.out.println(names);
+
+    }
+}
